5bbcbd0328cfd30cacd742a10c41938b54bbf06f
Ilya Skiba <hamurappi.tula@mail.ru>
2021-01-20 20:10:20 +0300

    Добавьте файлы проекта.

---
+++ Homework/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Array = new int[5];
+            int i,j,small_value;
+            int sum=0;
+            for (i = 0; i < Array.Length; i++)
+            {
+                Console.Write("Введите элемент массива "+i+": ");
+                Array[i]= int.Parse(Console.ReadLine());
+                Console.WriteLine();
+            }
+            // Вывод массива в обратном порядке
+            Console.Write("Массив в обратном порядке: ");
+            for (j = (Array.Length-1); j >= 0; j--)
+            {
+                Console.Write(Array[j]+" ");
+            }
+            Console.WriteLine();
+            // Найти сумму четных чисел в массиве
+            for (j = (Array.Length - 1); j >= 0; j--)
+            {
+                if (Array[j]%2==0)
+                {
+                    sum += Array[j];
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("Сумма чётных элементов в массиве равна: "+sum);
+            Console.WriteLine( );
+            // Найти наименьший член массива
+            small_value = Array[4];
+            for (j = (Array.Length - 1); j >= 0; j--)
+            {
+                if(Array[j]<=small_value)
+                small_value = Array[j];
+            }
+            Console.WriteLine("Наименьшее значение равно: "+small_value);
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_code_1
+{
+    class Test
+    {
+        public int a, b;
+        public Test(int i, int j)
+        {
+            a = i; b = j;
+        }
+        // Передать объект. Теперь переменные ob.a и ob.b из объекта, используемого в вызове метода, будут изменены.
+
+        public void Change(Test ob)
+        {
+            ob.a = ob.a + ob.b;
+            ob.b = - ob.b;
+        }
+    }
+    class CallByRef
+    {
+        static void Main(string[] args)
+        {
+            Test ob = new Test(15,20);
+            Console.WriteLine("ob.a и ob.b до вызова: "+ob.a+" "+ob.b);
+            ob.Change(ob);
+            Console.WriteLine("a и b после вызова: " + ob.a + " " + ob.b);
+        }
+    }
+}
